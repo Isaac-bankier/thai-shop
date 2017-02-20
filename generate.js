654c5a72373031b5f03de6937cd06d26027c790c
Isaac-bankier <isaacbankier@gmail.com>
2017-02-20 11:53:05 +1100

    Add files via upload
    Updated index gen.'s and material design
    
    TODO:
    add cart icon
    fix color scheme
    add cart to everything
    make more css puns

---
+++ generate.js
@@ -0,0 +1,14 @@
+var sel = document.getElementById('year');
+var currentTime = new Date();
+var currentyear = currentTime.getFullYear();
+var lowyear = currentyear - 123; //http://www.nbcnews.com/news/other/123-year-old-bolivian-man-oldest-living-person-ever-documented-f6C10934840
+
+for (i = lowyear; i < currentyear+1; i++) {
+  var opt = document.createElement('option');
+  opt.innerHTML = i;
+  opt.value = i;
+  if (i==currentyear-25){
+  	opt.selected = "selected";
+  }
+  sel.appendChild(opt);
+}
