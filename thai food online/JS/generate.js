ac4b6fa6ab4b06348752032a89214cf5368d4c16
Isaac Bankier <isaacbankier@gmail.com>
2017-03-19 00:06:07 +1100

    Add files via upload

--- thai food online/JS/generate.js
+++ thai food online/JS/generate.js
@@ -3,12 +3,12 @@ var currentTime = new Date();
 var currentyear = currentTime.getFullYear();
 var lowyear = currentyear - 123; //http://www.nbcnews.com/news/other/123-year-old-bolivian-man-oldest-living-person-ever-documented-f6C10934840
 
-for (i = lowyear; i < currentyear+1; i++) {
-  var opt = document.createElement('option');
-  opt.innerHTML = i;
-  opt.value = i;
-  if (i==currentyear-25){
-  	opt.selected = "selected";
-  }
-  sel.appendChild(opt);
+for (i = lowyear; i < currentyear + 1; i++) {
+    var opt = document.createElement('option');
+    opt.innerHTML = i;
+    opt.value = i;
+    if (i == currentyear - 25) {
+        opt.selected = "selected";
+    }
+    sel.appendChild(opt);
 }
--- thai food online/JS/scroll.js
+++ thai food online/JS/scroll.js
@@ -2,16 +2,16 @@
 
 var area = -1;
 
-function scroll_down(){
-	if (5>area){
-		 area += 1;
-  		window.location.hash = area;
-		}
+function scroll_down() {
+    if (5 > area) {
+        area += 1;
+        window.location.hash = area;
+    }
 }
 
-function scroll_up(){
-	if (area>-1){
- 	 area -= 1;
-  	window.location.hash = area;
-	}
+function scroll_up() {
+    if (area > -1) {
+        area -= 1;
+        window.location.hash = area;
+    }
 }
